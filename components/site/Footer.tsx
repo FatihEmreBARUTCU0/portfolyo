0579ee9dc287e488ecb4a0bc7d5cdbc3f577e9ea
Fatih Emre Barutcu <emrecompbarutcu@gmail.com>
2026-07-02 12:55:56 +0300

    Sync main with working master version

--- components/site/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./site.module.css";
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      © {new Date().getFullYear()} Fatih Emre Barutçu — Tüm hakları saklıdır.
-    </footer>
-  );
-}
